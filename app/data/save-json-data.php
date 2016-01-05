043c4fd921544844c6bce84e9c2caa4ac15ff07c
Aaron <aaronmcavinue@gmail.com>
2016-01-04 20:55:08 -0500

    Delete files

--- app/data/save-json-data.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $json_file = "todo-list-data.json";
-    $json_text = file_get_contents('php://input');
-    
-    if (file_put_contents($json_file, $json_text)) {
-        echo 1;
-    } else {
-        echo 0;
-    }
